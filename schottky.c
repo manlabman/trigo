cb782c0074181f93d8f0eddca5fafd107e3195d3
manlabman <manlabman@gmail.com>
2024-10-21 10:58:18 -0500

    Update schottky.c
    alfa_commit

--- schottky.c
+++ schottky.c
@@ -5,7 +5,12 @@ int main(){
   int r=5
   int s=567
   for(i=34;i<75;i++)
-    for/j=45;i<56;i++);
+    for(j=45;i<56;i++);
         for(k=58;i<76;j++);
+            for(l=12,l<12,l++);
+
+
+
+  
   
 }
